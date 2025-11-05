399fc067e42fddad7d34c2f0b5f0732329229135
Ashley Nagel <ashleynagel@Ashleys-MacBook-Air.local>
2025-11-04 20:28:31 -0500

    Pulled all NDAA amendment texts from Congress.gov

--- src/cdg.py
+++ src/cdg.py
@@ -26,17 +26,30 @@ class CDGClient:
         api_url,
         api_key,
         api_version,
-        response_format,
+        response_format='json',
+        limit=100,
+        offset=0
     ):
         self.base_url = urljoin(api_url, api_version) + "/"
+        self.limit = limit
+        self.response_format = response_format
+        self.offset = offset
         self._session = requests.Session()
 
         # do not use url parameters, even if offered, use headers
-        self._session.params = {"format": response_format}
         self._session.headers.update({"x-api-key": api_key})
         self._session.hooks = {
             "response": lambda r, *args, **kwargs: r.raise_for_status()
         }
+        self.update_params()
+
+    def update_params(self, response_format='', limit=0, offset=None):
+        self.response_format = response_format if response_format else self.response_format
+        self.limit = limit if limit else self.limit
+        self.offset = offset if offset is not None else self.offset
+        self._session.params = {"format": self.response_format,
+                                "limit": self.limit,
+                                "offset": self.offset}
 
     def __getattr__(self, method_name):
         """Find the session method dynamically and cache for later."""
@@ -50,21 +63,49 @@ class CDG:
         api_url,
         api_key,
         api_version,
-        response_format
     ):
        self.client = CDGClient(api_url,
                                api_key,
-                               api_version,
-                               response_format)
+                               api_version)
 
-    def get_bill_text(self, congress, bill_type, bill_num):
-        """ Returns a URL to the HTML or XML of the bill text """
-        endpoint = f"bill/{congress}/{bill_type}/{bill_num}/text"
+    def get_bill_details(self, congress, bill_type, bill_num):
+        endpoint = f"bill/{congress}/{bill_type}/{bill_num}"
         data, status_code = self.client.get(endpoint)
+        return data, status_code
+
+    def get_amendments(self, congress, bill_type, bill_num):
+        """ Returns a URL to the HTML or XML of the bill text """
+        offset = 0
+        total_count = 1
+        limit = self.client.limit
+        data = []
+        while offset < total_count:
+            self.client.update_params(offset=offset)
+            data_page, _ = self._get_amendments(congress, bill_type, bill_num)
+            data.extend(data_page['amendments'])
+            total_count = data_page['pagination']['count']
+            offset += limit
+        self.client.update_params(offset=0)
         return data
 
-    def get_amendment(client):
-        return
+    def _get_amendments(self, congress, bill_type, bill_num):
+        endpoint = f"bill/{congress}/{bill_type}/{bill_num}/amendments"
+        data, status_code = self.client.get(endpoint)
+        return data, status_code
 
-    def get_amendments(client):
-        return
+    def get_amendment_text(self, congress, amendment_type, amendment_num):
+        url = self._get_amendment_text_url(congress, amendment_type, amendment_num)
+        print(f'Getting text from {url}...')
+        if url:
+            response = requests.get(url, timeout=10)
+            return response.content.decode('utf-8')
+        return ''
+
+    def _get_amendment_text_url(self, congress, amendment_type, amendment_num):
+        endpoint = f"amendment/{congress}/{amendment_type}/{amendment_num}/text"
+        data, status_code = self.client.get(endpoint)
+        formats = data['textVersions'][0]['formats']
+        for format in formats:
+            if format['type'].upper() == 'HTML':
+                return format['url']
+        return ''
--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import streamlit as st
 from cdg import *
 from configparser import ConfigParser
 import json
+import os
 
 API_URL = "https://api.congress.gov/"
 API_VERSION = "v3"
-RESPONSE_FORMAT = "json" # can't get xml to work
 
 config = ConfigParser()
 config.read("../secrets.ini")
@@ -16,10 +16,52 @@ CONGRESS = "119"
 BILL_TYPE = "s"
 BILL_NUM = "2296"
 
+# Temporary files to avoid unnecessary API calls
+AMENDMENT_LIST_FILE = "amendment_list.txt"
+
 if __name__ == "__main__":
     cdg = CDG(api_url=API_URL,
               api_key=API_KEY,
-              api_version=API_VERSION,
-              response_format=RESPONSE_FORMAT)
-    data = cdg.get_bill_text(CONGRESS, BILL_TYPE, BILL_NUM)
-    print(json.dumps(data, indent=4))
+              api_version=API_VERSION)
+
+    # Check to see how many amendments exist for this bill
+    bill_details = cdg.get_bill_details(CONGRESS, BILL_TYPE, BILL_NUM)
+    amendment_count = bill_details[0]['bill']['amendments']['count']
+
+    # Check if we have a cached list already
+    old_amendment_count = 0
+    if os.path.exists(AMENDMENT_LIST_FILE):
+        try:
+            with open(AMENDMENT_LIST_FILE, 'r') as f:
+                amendments = json.load(f)
+                old_amendment_count = len(amendments)
+        except Exception as e:
+            print(f"ERROR opening amendments: {e}")
+
+    # There must be new amendments (or ones deleted?) so get the most updated list
+    # I don't assume that Congress.gov appends newest amendments to the end of the page,
+    # but I should check this at some point
+    if amendment_count != old_amendment_count:
+        amendments = cdg.get_amendments(CONGRESS, BILL_TYPE, BILL_NUM)
+
+        # Get amendment text for each amendment
+        retries = 3
+        for i, amendment in enumerate(amendments):
+            print(f"Processing amendment {i+1}...")
+            amendment_type = amendment['type'].lower()
+            amendment_num = amendment['number']
+            for _ in range(retries):
+                try:
+                    text = cdg.get_amendment_text(CONGRESS, amendment_type, amendment_num)
+                    amendments[i].update({'text': text})
+                    break
+                except Exception as e:
+                    print(f"ERROR on {i}: {e}")
+
+        # Dump the amendments with their text into the cache file
+        with open(AMENDMENT_LIST_FILE, 'w') as f:
+            json.dump(amendments, f, indent=4)
+
+
+
+
